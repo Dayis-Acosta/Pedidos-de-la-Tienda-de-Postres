d3d5ee5a9bc6228e81c220aa99c119acceee5edc
Dayanna Acosta Ledesma <est.dayanna.acosta@unimilitar.edu.co>
2026-03-19 10:15:20 -0500

    clases y atributos iniciales para el proyecto

---
+++ src/main/java/com/mycompany/tiendadepostres/Comanda.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.tiendadepostres;
+
+
+public class Comanda {
+    private int nmesa;
+    private int cantMesa;
+    
+    public Comanda(){}
+   
+   public int Mesa (int cantMesa){
+       //saber si hay mesas libres o no
+       return cantMesa;
+   }
+   public int Ingreso(int nmesa){
+       // Lugar donde se quiere sentar
+       return nmesa;
+   }
+}
